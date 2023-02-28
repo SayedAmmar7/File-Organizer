16f68087bc5dfce7ce4383e96bc5e4751bce5b99
SayedAmmar7 <sayedammarhassan@gmail.com>
2023-02-28 22:12:56 +0530

    Added tree.js

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 
 let helpfunction = require("./commands/help");
 let organizefunction = require("./commands/organize")
+let treefunction = require("./commands/tree");
 let inputArr = process.argv.slice(2);
 let command = inputArr[0];
 let path = inputArr[1];
 
 switch(command){
     case "tree":
-        
+        treefunction.tree(path);
       break;
     case "organize":
         organizefunction.organize(path);
